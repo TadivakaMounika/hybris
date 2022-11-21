e3138d0e38f8c7c93e109225a33a3150c0c81baa
Mounika <tadivaka.mounika@capgemini.com>
2022-11-21 23:16:02 +0530

    Added bin folder

---
+++ bin/custom/training/traininginitialdata/gensrc/com/hybris/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 14-Nov-2022, 9:30:22 PM                     ---
+ * ----------------------------------------------------------------
+ *  
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.hybris.training.initialdata.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
